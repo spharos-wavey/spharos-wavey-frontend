2bf1d96ea6bd4de0296018132315f5525d86b21b
Jason Ahn <beat1103@gmail.com>
2023-04-25 17:35:17 +0900

    feat: publish index

---
+++ components/layouts/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { headerMenuData } from '@/datas/staticMenuDatas'
+import HeaderMenuItem from './HeaderMenuItem'
+import { headerMenuType } from '@/types/headerType'
+import style from '@/components/layouts/Header.module.css'
+
+export default function Header() {
+  return (
+    <header className={style.headerContainer}>
+      <nav>
+        <ul>
+          {
+            headerMenuData.map((menuItem:headerMenuType) => {
+              return (
+                <HeaderMenuItem 
+                  menuItem = {menuItem}
+                  key={menuItem.id}
+                />
+              )
+            })
+          }
+        </ul>
+      </nav>
+    </header>
+  )
+}
---
+++ components/pages/main/EventBanner.tsx
@@ -0,0 +1,44 @@
+import { eventBannerType } from '@/types/eventBannerType'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import { mainEventData } from '@/datas/mainEventData'
+import style from '@/components/pages/main/EventBanner.module.css'
+
+export default function EventBanner() {
+
+  const [bannerData, setBannerData] = useState<eventBannerType>();
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  useEffect(()=>{
+    // todo: banner image load for fetch
+    setBannerData(mainEventData)
+  },[])
+  return (
+    <div className={isActive ? `${style.bannerWrap} ${style.active}`: style.bannerWrap} onClick={()=>setIsActive(!isActive)}>
+      {
+        bannerData && 
+        <>
+          <div className={style.bannerContents}>
+            <div className={style.bannerLeft}>
+              <h3>{bannerData.carName}</h3>
+              <div className={style.brandImage}>
+                <Image src={bannerData.carBrandImage} width={200} height={200} alt={bannerData.carBrand} priority/>
+              </div>
+            </div>
+            <div className={style.bannerRight}>
+              <Image src={bannerData.carImage} width={426} height={244} alt={bannerData.carName} priority/>
+            </div>
+          </div>
+          <div className={style.bannerOverlay}>
+            <div className={style.bannerText}>
+              지금 당장 확 마 빌리 타 뿌까? 
+            </div>
+            <div className={style.bannerButton}>
+              예약하기
+            </div>
+          </div>
+        </>
+      }
+    </div>
+  )
+}
