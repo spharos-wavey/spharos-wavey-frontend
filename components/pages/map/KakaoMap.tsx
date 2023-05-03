65a65e7cfce382853ac0d43b244d2ac1ee8a16d1
ys <110089522+oasis0981@users.noreply.github.com>
2023-05-03 15:35:51 +0900

    Feat: /map 페이지에 카카오맵 생성

---
+++ components/pages/map/KakaoMap.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+
+export default function KakaoMap() {
+  return (
+    <>
+      <Map
+        center={{ lat: 33.5563, lng: 126.79581 }}
+        style={{ width: "100%", height: "360px" }}
+      >
+        <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+          <div style={{ color: "#000" }}>Hello World!</div>
+        </MapMarker>
+      </Map>
+    </>
+  );
+}
---
+++ pages/map/index.jsx
@@ -0,0 +1,6 @@
+import KakaoMap from "@/components/pages/map/KakaoMap";
+import React, { useEffect } from "react";
+
+export default function Map() {
+  return <KakaoMap />;
+}
