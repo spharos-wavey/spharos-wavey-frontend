19c5972c75d29d07a5b94eaa500c58705bdc12da
MeSawSomething <miso.hong.1989@gmail.com>
2023-05-18 16:22:31 +0900

    Feat: 사이드메뉴 애니메이션 일부 수정

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,11 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { headerMenuData } from "@/datas/staticMenuDatas";
 import style from "@/components/layouts/Header.module.css";
 import MenuItem from "./MenuItem";
 import ModalSideBar from "../modals/ModalSideBar";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { carDataType } from "@/types/carDataType";
+
 
 
 export default function Header() {
+  const [carData, setCarData] = useState<carDataType>();
+  const router = useRouter();
+  console.log(router.query.cid);
+
+  if (router.query.cid !== undefined) {
+    const getData = async () => {
+      const result = await axios.get(
+        `https://api-billita.xyz/vehicle/${router.query.cid}`
+      );
+      console.log("data : ", result.data);
+      console.log("img url : ", result.data.frameInfo.image);
+      setCarData(result.data);
+    };
+    getData();
+  }
+
+
   const [isSideOpen, setIsSideOpen] = useState(false);
   console.log("isOpen", isSideOpen);
 
