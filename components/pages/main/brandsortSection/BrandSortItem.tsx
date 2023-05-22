d2d59443f7a18360c55be15fff9d0dff4c84b436
MeSawSomething <miso.hong.1989@gmail.com>
2023-05-22 20:40:52 +0900

    Feat: Footer map icon replaced

--- components/pages/main/brandsortSection/BrandSortItem.tsx
+++ components/pages/main/brandsortSection/BrandSortItem.tsx
@@ -1,24 +1,33 @@
-import style from "@/components/pages/main/brandsortSection/BrandSortItem.module.css"
-import { brandSortType } from "@/types/brandSortType"
-import Image from 'next/image'
-import { useRouter } from "next/router"
+import style from "@/components/pages/main/brandsortSection/BrandSortItem.module.css";
+import { brandSortType } from "@/types/brandSortType";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
-export default function BrandSortItem(props: {item: brandSortType}) {
+export default function BrandSortItem(props: { item: brandSortType }) {
+  const router = useRouter();
 
-  const router = useRouter()
-  
   const handleSortbyBrand = () => {
-    router.push(`/car/brand?brandName=${props.item.name}`)
-  }
+    router.push(`/car/brand?brandName=${props.item.name}`);
+  };
 
-  console.log(props.item)
-  
+  console.log(props.item);
+
   return (
     <div className={style.brandWrap}>
       <div className={style.circlePad} onClick={handleSortbyBrand}>
-        <Image src={props.item.icon !== undefined ? props.item.icon : '/assets/images/common/billitaLogoOnly.svg'} width={40} height={40} alt={props.item.name} priority />
+        <Image
+          src={
+            props.item.icon !== undefined
+              ? props.item.icon
+              : "/assets/images/common/billitaLogoOnly.svg"
+          }
+          width={40}
+          height={40}
+          alt={props.item.name}
+          priority
+        />
       </div>
       <div className={style.brandName}>{props.item.name}</div>
     </div>
-  )
+  );
 }
