0c6ea702ee2db2f1543736e4fd76f4d6001f6ef0
MeSawSomething <miso.hong.1989@gmail.com>
2023-05-24 17:34:39 +0900

    Feat: BookList Data fetch trial

--- pages/license/index.tsx
+++ pages/license/index.tsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { use, useState } from "react";
+import { useRouter } from "next/router";
 import SimpleBackLayout from "@/components/layouts/simpleBack/SimpleBackLayout";
-import SectionTitle from "@/components/ui/SectionTitle";
-import LicenseType from "../../components/pages/license/LicenseTop";
-import PersonalInfo from "../../components/pages/license/LicenseBottom";
 import BottomFixedContainer from "@/components/layouts/BottomFixedContainer";
 import Button from "@/components/ui/Button";
 import LicenseWrapper from "@/components/pages/license/LicenseWrapper";
 
 export default function License() {
+  const router = useRouter();
+  const [extractedPageNo, setExtractedPageNo] = useState<string>("");
+  const handleRedirectPrevPage = () => {
+    sessionStorage.setItem("userLicense", "true");
+    router.push(sessionStorage.getItem("carDetail") as string);
+    alert("뀨");
+  };
+  
+  const extractIdForBookPage = sessionStorage.getItem("carDetail")?.split("/")[2];
+  const handleOpenBookPage = () => {
+    router.push(`rental/${extractIdForBookPage}`);
+  }
+
+  const handleRegister = () => {
+    console.log(extractIdForBookPage);
+    sessionStorage.getItem("userLicense")
+      ? handleOpenBookPage()
+      : handleRedirectPrevPage();
+  };
   return (
-  <main>
-    <section>
-      <div>
-        <LicenseWrapper />
-      </div>
+    <main>
+      <section>
+        <div>
+          <LicenseWrapper />
+        </div>
+      </section>
       <BottomFixedContainer>
-        <Button btnType={"button"} btnEvent={() => alert("g")} shadow={true}>
+        <Button
+          btnType={"button"}
+          btnEvent={() => handleRegister()}
+          shadow={true}
+        >
           등록하기
         </Button>
       </BottomFixedContainer>
-    </section>
-  </main>
+    </main>
   );
 }
 
