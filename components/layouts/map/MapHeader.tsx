539ec97fdc339d02ad2a9121730804158f20e53a
Jason Ahn <beat1103@gmail.com>
2023-05-25 17:59:22 +0900

    feat:map

--- components/layouts/map/MapHeader.tsx
+++ components/layouts/map/MapHeader.tsx
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./MapHeader.module.css";
 import { mapHeaderIcon } from "@/datas/staticMenuDatas";
 import MenuItem from "../MenuItem";
+import { useRouter } from "next/router";
 
 export default function MapHeader() {
+
+  const router = useRouter();
+  const [isActive, setIsActive] = useState<boolean>(true);
+  const handleLink = (path:string) => {
+    router.push(path);
+  }
+
+  // get touch event
+  useEffect(() => {
+    const handleTouch = (e: TouchEvent) => {
+      if (e.touches[0].clientY > 100) {
+        setIsActive(true);
+      } 
+    };
+    window.addEventListener("touchmove", handleTouch);
+    return () => {
+      window.removeEventListener("touchmove", handleTouch);
+    };
+  }, []);
+
+  // interval 3s for slide down
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsActive(false);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  
   return (
-    <div className={style.header}>
+    <div className={isActive ? `${style.header} ${style.close}` : style.header}>
       <nav>
         <ul>
-          <MenuItem
-            menuItem={mapHeaderIcon[0]}
-            key={mapHeaderIcon[0].id}
-            width="1rem"
-          />
-          <MenuItem
-            menuItem={mapHeaderIcon[1]}
-            key={mapHeaderIcon[1].id}
-            width="5rem"
-          />
-          <MenuItem
-            menuItem={mapHeaderIcon[2]}
-            key={mapHeaderIcon[2].id}
-            width="1.5rem"
-          />
+          {
+            mapHeaderIcon.map((item)=>{
+              return (
+                <MenuItem
+                  menuItem={item}
+                  key={item.id}
+                  onClick={()=>handleLink(item.path)}
+                />
+              )
+            })
+          }
         </ul>
       </nav>
     </div>
