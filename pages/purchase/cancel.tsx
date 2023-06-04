269949c863ecc3728e9667e9fdaa52d53ba38bd4
MeSawSomething <miso.hong.1989@gmail.com>
2023-06-05 01:23:29 +0900

    Style: created purchase/fail & cancel page

--- pages/purchase/cancel.tsx
+++ pages/purchase/cancel.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import router from 'next/router'
+import PageLoader from '@/components/ui/PageLoader'
 
 export default function cancel() {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      router.push('/'); 
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [router]);
   return (
-    <div>
-      결제를 취소하셨습니다
-    </div>
+    <PageLoader text="결제를 취소 하셨습니다. 메인화면으로 이동합니다." />
   )
 }
