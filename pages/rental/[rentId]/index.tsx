5b05dc202b3b028c0b5351427e1f77b134bf9e19
Jason Ahn <beat1103@gmail.com>
2023-06-06 22:41:30 +0900

    fix: get rent data

--- pages/rental/[rentId]/index.tsx
+++ pages/rental/[rentId]/index.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SimpleBackLayout from "@/components/layouts/simpleBack/SimpleBackLayout";
 import RentalWrapper from "@/components/pages/rental/RentalWrapper";
+import { useRecoilState } from "recoil";
+import { authState } from "@/state/authState";
+import AuthRecoilChecker from "@/components/util/AuthRecoilChecker";
 
 export default function Detail() {
+  
+  const [auth, setAuth] = useRecoilState(authState);
+
+  useEffect(() => {
+    if (!auth.auth && AuthRecoilChecker()&&typeof window !== 'undefined') {
+      setAuth({
+        auth: true, 
+        token: localStorage.getItem("token") as string, 
+        uid: localStorage.getItem("uid") as string, 
+        nickName: localStorage.getItem("nickName") as string,
+        email: localStorage.getItem("email") as string,
+        profileImageUrl: localStorage.getItem("profileImageUrl") as string,
+      });
+    }
+  }, []);
 
   return <main> <RentalWrapper /></main>;
 }
